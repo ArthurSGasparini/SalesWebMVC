dcb7d9a1a262e96e1f9b9f629b7e7af52608f41f
Arthur <rthurgmer3@gmail.com>
2026-05-08 20:06:04 -0300

    SellersController

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index() // Nova subpasta chamada Sellers, com a ação Index, que é a ação padrão para exibir uma lista de vendedores. Atualmente, o método retorna apenas a view correspondente, mas futuramente pode ser expandido para incluir lógica de consulta ao banco de dados e exibição dos vendedores cadastrados.
+        {
+            return View();
+        }
+    }
+}
