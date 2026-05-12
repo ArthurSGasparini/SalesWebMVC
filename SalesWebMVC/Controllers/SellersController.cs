fd554db1f5504bfb06a4a6709b9a9a8204cb3fa0
Arthur <rthurgmer3@gmail.com>
2026-05-12 13:49:13 -0300

    Delete seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -32,10 +32,37 @@ namespace SalesWebMVC.Controllers
 
         [HttpPost] // A anotação [HttpPost] indica que este método deve ser chamado quando uma solicitação HTTP POST for feita para a ação Create. Isso é típico para ações que processam formulários de criação ou edição de dados.
         [ValidateAntiForgeryToken] // A anotação [ValidateAntiForgeryToken] é usada para proteger contra ataques de falsificação de solicitação entre sites (CSRF). Ela garante que o formulário enviado para esta ação contenha um token de validação que corresponda ao token gerado pelo servidor, ajudando a prevenir ataques maliciosos.
-        public IActionResult Create(Seller seller) 
+        public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            } 
+            return View(obj);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
