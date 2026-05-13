35a78f7b675ed14c1f734f75c20965b98b254807
Arthur <rthurgmer3@gmail.com>
2026-05-13 09:38:43 -0300

    Update seller and custom service exception

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc; //controlador recebe a chamada do caminho /sellers do site, e a ação Index é responsável por processar essa solicitação e retornar a view correspondente.
 using SalesWebMVC.Models;
 using SalesWebMVC.Models.ViewModels;
-using SalesWebMVC.Services;     //A view é um arquivo de interface do usuário que exibe a lista de vendedores cadastrados no sistema. No momento, o método Index retorna apenas a view,
-                                //mas futuramente pode ser expandido para incluir lógica de consulta ao banco de dados e exibição dos vendedores cadastrados.
+using SalesWebMVC.Services;
+using SalesWebMVC.Services.Exceptions;
+using System.Collections.Generic;     //A view é um arquivo de interface do usuário que exibe a lista de vendedores cadastrados no sistema. No momento, o método Index retorna apenas a view,
+                                      //mas futuramente pode ser expandido para incluir lógica de consulta ao banco de dados e exibição dos vendedores cadastrados.
 
 namespace SalesWebMVC.Controllers
 {
@@ -40,14 +42,14 @@ namespace SalesWebMVC.Controllers
 
 
 
-        public IActionResult Delete(int? id)
+        public IActionResult Delete(int? id) // A ação Delete é responsável por exibir uma confirmação de exclusão para um vendedor específico. Ela recebe um parâmetro id que representa o identificador do vendedor a ser excluído. O método verifica se o id é nulo e, em caso afirmativo, retorna uma resposta NotFound. Em seguida, ele busca o vendedor correspondente ao id usando o serviço _sellerService.FindById(id.Value). Se o vendedor não for encontrado, novamente retorna NotFound. Caso contrário, retorna a view com o objeto do vendedor para exibir a confirmação de exclusão.
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var obj = _sellerService.FindById(id.Value);
+            var obj = _sellerService.FindById(id.Value); // O método FindById é chamado para buscar o vendedor correspondente ao id fornecido. O valor do id é passado como argumento para o método, e o resultado é armazenado na variável obj. Se o vendedor não for encontrado, obj será nulo.
             if (obj == null)
             {
                 return NotFound();
@@ -58,13 +60,13 @@ namespace SalesWebMVC.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete(int id)
+        public IActionResult Delete(int id) // A ação Delete é responsável por processar a exclusão de um vendedor específico. Ela recebe um parâmetro id que representa o identificador do vendedor a ser excluído. O método chama o serviço _sellerService.Remove(id) para remover o vendedor do banco de dados. Após a exclusão, ele redireciona para a ação Index para exibir a lista atualizada de vendedores.
         {
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Details(int? id)
+        public IActionResult Details(int? id) // A ação Details é responsável por exibir os detalhes de um vendedor específico. Ela recebe um parâmetro id que representa o identificador do vendedor cujos detalhes devem ser exibidos. O método verifica se o id é nulo e, em caso afirmativo, retorna uma resposta NotFound. Em seguida, ele busca o vendedor correspondente ao id usando o serviço _sellerService.FindById(id.Value). Se o vendedor não for encontrado, novamente retorna NotFound. Caso contrário, retorna a view com o objeto do vendedor para exibir os detalhes.
         {
             if (id == null)
             {
@@ -78,7 +80,44 @@ namespace SalesWebMVC.Controllers
             return View(obj);
         }
 
-        
+        public IActionResult Edit(int? id) // A ação Edit é responsável por exibir um formulário para editar um vendedor existente. Ela recebe um parâmetro id que representa o identificador do vendedor a ser editado. O método verifica se o id é nulo e, em caso afirmativo, retorna uma resposta NotFound. Em seguida, ele busca o vendedor correspondente ao id usando o serviço _sellerService.FindById(id.Value). Se o vendedor não for encontrado, novamente retorna NotFound. Caso contrário, ele prepara uma lista de departamentos usando _departmentService.FindAll() e cria um objeto SellerFormViewModel que contém o vendedor e a lista de departamentos. Por fim, retorna a view com o viewModel para exibir o formulário de edição.
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
 
+        }
+
+        [HttpPost] // A anotação [HttpPost] indica que este método deve ser chamado quando uma solicitação HTTP POST for feita para a ação Edit. Isso é típico para ações que processam formulários de criação ou edição de dados.
+        [ValidateAntiForgeryToken]// A anotação [ValidateAntiForgeryToken] é usada para proteger contra ataques de falsificação de solicitação entre sites (CSRF). Ela garante que o formulário enviado para esta ação contenha um token de validação que corresponda ao token gerado pelo servidor, ajudando a prevenir ataques maliciosos.
+        public IActionResult Edit(int id, Seller seller) // A ação Edit é responsável por exibir um formulário para editar um vendedor existente. Ela recebe um parâmetro id que representa o identificador do vendedor a ser editado. O método verifica se o id é nulo e, em caso afirmativo, retorna uma resposta NotFound. Em seguida, ele busca o vendedor correspondente ao id usando o serviço _sellerService.FindById(id.Value). Se o vendedor não for encontrado, novamente retorna NotFound. Caso contrário, ele prepara uma lista de departamentos usando _departmentService.FindAll() e cria um objeto SellerFormViewModel que contém o vendedor e a lista de departamentos. Por fim, retorna a view com o viewModel para exibir o formulário de edição.
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index)); // Após a atualização do vendedor, o método redireciona para a ação Index para exibir a lista atualizada de vendedores. O método RedirectToAction é usado para redirecionar para a ação Index, que é a ação responsável por exibir a lista de vendedores cadastrados no sistema.
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
